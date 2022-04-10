7bc3cdc322acbcb8e448e31f2c87b51ce3a3ac4c
9bany <bany.y0599@gmail.com>
2022-04-10 18:20:13 +0700

    simple_bank: add config file for main

--- main.go
+++ main.go
@@ -3,25 +3,24 @@ package main
 import (
 	"9bany/simple_bank/api"
 	db "9bany/simple_bank/db/sqlc"
+	"9bany/simple_bank/util"
 	"database/sql"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	address  = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalln("Can not load config file: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Can not connect to database:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NerServer(store)
-	server.Start(address)
+	server.Start(config.ServerAddress)
 }
